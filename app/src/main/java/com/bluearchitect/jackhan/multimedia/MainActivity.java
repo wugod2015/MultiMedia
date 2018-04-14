86212f87944d4903bbf6b3cb5feb8c94f9ae621c
hhz <2579546054@qq.com>
2018-04-14 15:03:10 +0800

    优化ScrollView嵌套问题

--- app/src/main/java/com/bluearchitect/jackhan/multimedia/MainActivity.java
+++ app/src/main/java/com/bluearchitect/jackhan/multimedia/MainActivity.java
@@ -1,24 +1,19 @@
 package com.bluearchitect.jackhan.multimedia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.yckj.hhz.multimedialibrary.SelectImgGridView;
-import com.yckj.hhz.multimedialibrary.SelectImgGridView1;
-import com.yckj.hhz.multimedialibrary.SelectImgGridView2;
-
-import cn.finalteam.rxgalleryfinal.rxbus.event.ImageMultipleResultEvent;
-import cn.finalteam.rxgalleryfinal.rxbus.event.ImageRadioResultEvent;
 
 public class MainActivity extends AppCompatActivity {
 
-    SelectImgGridView1 selectImgGridView;
+    SelectImgGridView selectImgGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*selectImgGridView = (SelectImgGridView1) findViewById(R.id.selectImgGridView);
+        /*selectImgGridView = (SelectImgGridView) findViewById(R.id.selectImgGridView);
         selectImgGridView.init();
         selectImgGridView.getSelectedPathList();
         selectImgGridView.setSelectImgListener(new SelectImgGridView.SelectImgListener() {
