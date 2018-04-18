033d7454add98e757e7ce7a4fe459d82e2664e87
hhz <2579546054@qq.com>
2018-04-18 17:10:14 +0800

    截图

--- app/src/main/java/com/bluearchitect/jackhan/multimedia/MainActivity.java
+++ app/src/main/java/com/bluearchitect/jackhan/multimedia/MainActivity.java
@@ -4,17 +4,42 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.yckj.hhz.multimedialibrary.SelectImgGridView;
+import com.yckj.hhz.multimedialibrary.SelectImgRecyclerView;
+
+import java.io.File;
+import java.util.List;
+
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageMultipleResultEvent;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageRadioResultEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     SelectImgGridView selectImgGridView;
+    SelectImgRecyclerView selectImgRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         selectImgGridView = (SelectImgGridView) findViewById(R.id.selectImgGridView);
-        selectImgGridView.setMaxCount(12);
+//        selectImgGridView.setMaxCount(12);
+        selectImgRecyclerView = (SelectImgRecyclerView) findViewById(R.id.selectImgRecyclerView);
+//        selectImgGridView.setMaxCount(12);
+//        selectImgGridView.setIsCrop(true);
+//        selectImgGridView.setSpanCount(4);
+//        selectImgGridView.setSelectImgListener(new SelectImgGridView.SelectImgListener() {
+//            @Override
+//            public void onOneResult(ImageRadioResultEvent resultEvent) {
+//
+//            }
+//
+//            @Override
+//            public void onMultipleResult(ImageMultipleResultEvent resultEvent) {
+//
+//            }
+//        });
+//        List<String> pathList = selectImgRecyclerView.getSelectedPathList();
+//        List<File> fileList = selectImgRecyclerView.getSelectedFileList();
         /*selectImgGridView.init();
         selectImgGridView.getSelectedPathList();
         selectImgGridView.setSelectImgListener(new SelectImgGridView.SelectImgListener() {
